c1fb54c0f9982484523acb3fc9f9b8bc6ddaf64b
ay66866 <616263673@qq.com>
2021-06-10 10:40:25 +0800

    06.10

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,27 @@
 <template>
-  <h2>首页</h2>
+  <div>
+    <nav-bar class="home-nav">
+      <template v-slot:center>购物街</template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from '../../components/common/navbar/NavBar.vue';
+import {getHomeMultidata} from "../../network/request"
+
 export default {
-  name: "Home"
-}
+  name: "Home",
+  components: {
+    NavBar,getHomeMultidata
+  },
+};
 </script>
 
 
 <style>
+.home-nav {
+  background-color: var(--color-tint);
+  color:#fff
+}
 </style>
