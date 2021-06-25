351ae04d3ef657df1905e0a9dcbcdf4580e9b805
dw <793318828@qq.com>
2021-06-25 18:34:52 +0800

    06.25

--- src/components/content/tabControl/TabControl.vue
+++ src/components/content/tabControl/TabControl.vue
@@ -1,34 +1,61 @@
 <template>
   <div class="tab-control">
-      <div v-for="item in titles" :key="item" class="tab-control-item">
-          <span>{{item}}</span>
-      </div>
+    <!-- index是索引 -->
+    <div
+      v-for="(item, index) in titles"
+      :key="item"
+      class="tab-control-item"
+      :class="{ active: index === currentIndex }"
+      @click="itemClick(index)"
+    >
+      <span>{{ item }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "TabControl",
-    props: {
-        titles: {
-            type: Array,
-            default() {
-                return []
-            }
-        }
-    }
-}
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+    },
+  },
+};
 </script>
 
 <style>
-    .tab-control {
-        display: flex;
-        text-align: center;
-        font-size: 15px;
-    }
-    .tab-control-item {
-        flex: 1;
-        height: 40px;
-        line-height: 44px;
-    }
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  height: 40px;
+  line-height: 40px;
+  background-color: #fff;
+}
+.tab-control-item {
+  flex: 1;
+}
+.tab-control-item span {
+  padding: 5px;
+}
+.active {
+  color: var(--color-high-text);
+}
+.active span {
+  border-bottom: 3px solid var(--color-tint);
+}
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,9 +6,9 @@
     <home-swiper :banners="banners" />
     <recommend-view :recommends="recommends" />
     <feature-view />
-    <tab-control :titles="['流行', '新款', '精选']" />
-
-    <ul>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"/>
+    <goods-list :goods="goods[currentType].list" />
+    <!-- <ul>
       <li></li>
       <li></li>
       <li></li>
@@ -59,20 +59,21 @@
       <li></li>
       <li></li>
       <li></li>
-    </ul>
+    </ul> -->
   </div>
-  
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar.vue";
-import TabControl from "components/content/tabControl/TabControl"
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from 'components/content/goods/GoodsList.vue';
 
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView.vue";
 import FeatureView from "./childComps/FeatureView.vue";
 
-import { getHomeMultidata } from "../../network/home";
+import { getHomeMultidata, getHomeGoods } from "../../network/home";
+
 // import Swiper from '../../components/common/swiper/Swiper.vue';
 // import SwiperItem from '../../components/common/swiper/SwiperItem.vue'
 
@@ -84,19 +85,57 @@ export default {
     HomeSwiper,
     RecommendView,
     FeatureView,
+    GoodsList,
   },
   data() {
     return {
       banners: [],
       recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      currentType: 'pop'
     };
   },
   created() {
-    getHomeMultidata().then((res) => {
+    this.getHomeMultidata()
+    
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    // 时间监听
+    tabClick(index) {
+      switch(index) {
+        case 0:
+          this.currentType = "pop"
+          break
+        case 1:
+          this.currentType = "new"
+          break
+        case 2:
+          this.currentType = "sell"
+          break
+      }
+    },
+
+    //网络请求相关
+    getHomeMultidata(){
+      getHomeMultidata().then(res => {
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
-    });
-  },
+      })
+    },
+    getHomeGoods(type){
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list)
+      })
+    }
+  }
 };
 </script>
 
@@ -113,6 +152,11 @@ export default {
   left: 0;
   right: 0;
   top: 0;
+  z-index: 100;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
   z-index: 100;
 }
 </style>
