50c99be59d9d25de4481df2926faf7a70b0b3a6f
dw <793318828@qq.com>
2021-09-14 18:09:43 +0800

    0914

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,70 +1,84 @@
 <!--  -->
 <template>
   <div id="detail">
-    <detail-nav-bar class="detail-nav"/>
-    <scroll class="content">
-      <detail-swiper  :top-images="topImages" />
-      <detail-base-info :goods="goods"/>
-      <detail-shop-info :shop="shop"/>
-      <detail-goods-info :detail-info="detailInfo"/>
+    <detail-nav-bar class="detail-nav" />
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages" />
+      <detail-base-info :goods="goods" />
+      <detail-shop-info :shop="shop" />
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad" />
+      <detail-params-info :param-info="paramInfo" />
     </scroll>
   </div>
 </template>
 
 <script>
-import DetailNavBar from './childComps/DetailNavBar.vue'
-import DetailSwiper from './childComps/DetailSwiper.vue'
-import DetailBaseInfo from './childComps/DetailBaseInfo.vue'
-import DetailShopInfo from './childComps/DetailShopInfo.vue'
-import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+import DetailNavBar from "./childComps/DetailNavBar.vue";
+import DetailSwiper from "./childComps/DetailSwiper.vue";
+import DetailBaseInfo from "./childComps/DetailBaseInfo.vue";
+import DetailShopInfo from "./childComps/DetailShopInfo.vue";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+import DetailParamsInfo from "./childComps/DetailParamsInfo.vue"
 
-import Scroll from 'components/common/scroll/Scroll'
+import Scroll from "components/common/scroll/Scroll";
 
-import {getDetail, Goods, Shop} from '../../network/detail'
-
+import { getDetail, Goods, Shop, GoodsParam} from "../../network/detail";
 
 export default {
-  name: 'Detail',
+  name: "Detail",
   components: {
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
     DetailShopInfo,
     DetailGoodsInfo,
-    Scroll,
+    DetailParamsInfo,
+    Scroll
   },
   data() {
     return {
-      iid:null,
+      iid: null,
       topImages: [],
       goods: {},
       shop: {},
-      detailInfo: {}
-    }
+      detailInfo: {},
+      paramInfo: {}
+    };
   },
   created() {
     // 保存存入的iid
-    this.iid = this.$route.params.iid
-    
+    this.iid = this.$route.params.iid;
+
     // 根据iid请求详情数据
-    getDetail(this.iid).then(res => {
+    getDetail(this.iid).then((res) => {
       // 1.根据顶部的图片轮播数据
       console.log(res);
       const data = res.result;
-      this.topImages = data.itemInfo.topImages
+      this.topImages = data.itemInfo.topImages;
 
       // 2.获取商品信息
-      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
 
       // 3.创建店铺信息的对象
-      this.shop = new Shop(data.shopInfo)
+      this.shop = new Shop(data.shopInfo);
 
       // 4.保存商品的详情数据
-      this.detailInfo = data.detailInfo
-    })
-  }
-}
+      this.detailInfo = data.detailInfo;
 
+      // 5.获取参数信息
+      this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+    });
+  },  
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh()
+    }
+  }
+};
 </script>
 <style scoped>
 #detail {
